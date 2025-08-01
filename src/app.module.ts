ba5469e52c0bc0c7f4c4ce0131ffe8f967f747ea
rrsrrsrajeev <rrsrrsrajeev@gmail.com>
2025-08-01 14:44:27 +0530

    change

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 // import { AppController } from './app.controller';
 // import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+// import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
-import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [UserModule, AuthModule],
-  providers: [PrismaService],
+  imports: [UserModule, AuthModule, PrismaModule],
+  // providers: [PrismaService],
   // controllers: [AppController],// controllrer is user handle response and request
   // providers: [AppService],// service is business logic
 })
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,13 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
-  @Post('signup')
-  signup() {
-    return 'signup';
-  }
+  constructor(private authService: AuthService) {}
 
-  @Post('signin')
-  signin() {
-    return 'signin';
+  @Post('signup')
+  signup(@Body() body: { email: string; password: string; fullName: string }) {
+    console.log('body', body);
+    // return this.authService.signup(body.email, body.password, body.fullName);
   }
 }
